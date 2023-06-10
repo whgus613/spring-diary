31fcb149d3ac87259dec7cdd291fb8a3f96636bf
whgus613 <79959287+whgus613@users.noreply.github.com>
2023-06-10 16:45:32 +0900

    setting

---
+++ diaryMVC/src/main/java/kr/ac/jejunu/diarymvc/DiaryMvcApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.diarymvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryMvcApplication.class, args);
+    }
+
+}
---
+++ diaryMVC/src/test/java/kr/ac/jejunu/diarymvc/DiaryMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.diarymvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
